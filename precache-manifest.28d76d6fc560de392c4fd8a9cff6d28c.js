0cc1dbb4dedbe8705bff505def255410d10059a2
Your Name <quiramapino47@gmail.com>
2020-07-01 14:34:09 -0500

    Updates

---
+++ precache-manifest.28d76d6fc560de392c4fd8a9cff6d28c.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fe001a0b8de878203ff3f4f3e9fc044",
+    "url": "/PortafolioFinal/index.html"
+  },
+  {
+    "revision": "dfa479a827c2bf3adc05",
+    "url": "/PortafolioFinal/static/css/main.9727f6b7.chunk.css"
+  },
+  {
+    "revision": "4cd0a13fad77feffcaaa",
+    "url": "/PortafolioFinal/static/js/2.3585172e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/PortafolioFinal/static/js/2.3585172e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfa479a827c2bf3adc05",
+    "url": "/PortafolioFinal/static/js/main.546f0d5c.chunk.js"
+  },
+  {
+    "revision": "19cbbb90497a0394da44",
+    "url": "/PortafolioFinal/static/js/runtime-main.ef6d2a7c.js"
+  },
+  {
+    "revision": "3207c0bd51d3f3506ed1bc5569a122af",
+    "url": "/PortafolioFinal/static/media/Foto.3207c0bd.png"
+  },
+  {
+    "revision": "c2d32b96dad59f77db1c3b28885dea4e",
+    "url": "/PortafolioFinal/static/media/Marea.c2d32b96.jpg"
+  },
+  {
+    "revision": "7dbf79e7c8692e4c49e4086f66e88e16",
+    "url": "/PortafolioFinal/static/media/email.7dbf79e7.jpg"
+  },
+  {
+    "revision": "8bf660eca73737d5d371033f53961d05",
+    "url": "/PortafolioFinal/static/media/emisora.8bf660ec.jpg"
+  },
+  {
+    "revision": "d185625bad962195ff3a623ae18f6565",
+    "url": "/PortafolioFinal/static/media/jhonatan.d185625b.pdf"
+  },
+  {
+    "revision": "d3797688a34a80a4031a75fa00dd8729",
+    "url": "/PortafolioFinal/static/media/juego.d3797688.jpg"
+  },
+  {
+    "revision": "862ea93fe26a26553e45806777c69b61",
+    "url": "/PortafolioFinal/static/media/proyecto1.862ea93f.jpg"
+  },
+  {
+    "revision": "bc07217b58e8ab17f80c57baca20409b",
+    "url": "/PortafolioFinal/static/media/proyecto2.bc07217b.jpg"
+  },
+  {
+    "revision": "9750fe127db9d915dfc6c761e766f6b2",
+    "url": "/PortafolioFinal/static/media/proyecto3.9750fe12.jpg"
+  },
+  {
+    "revision": "898ad010ccef120bae4fce4f70336a3b",
+    "url": "/PortafolioFinal/static/media/proyecto4.898ad010.jpg"
+  }
+]);
